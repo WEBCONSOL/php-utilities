b48c10ecdb5f1f58f8db6d140a1e447037e752d5
WEBCONSOL Inc <info@webconsolinc.com>
2019-09-04 08:54:05 -0400

    enhanced

--- src/WC/Utilities/Mailer/Mail.php
+++ src/WC/Utilities/Mailer/Mail.php
@@ -2,6 +2,7 @@
 
 namespace WC\Utilities\Mailer;
 
+use HandlebarsHelpers\Hbs;
 use WC\Models\ListModel;
 use WC\Utilities\CustomResponse;
 
@@ -12,29 +13,51 @@ final class Mail
     const KEY_TO_EMAIL = "email";
     const KEY_TO_NAME = "name";
     private $envelop;
-    private $gx2cmsTmpl = true;
+    private $templatingEngine = '';
+    private $data = [];
     private $missingData = false;
+    private $opensslPkcs7Encrypt = false;
+    private $sslKey = '';
+    private $sslCertificate = '';
+    private $privateKeyPassword = ''; // optional
+    private $sslCertChain = ''; // optional
 
-    public function __construct(array $to, string $tpl, array $data, string $subject, array $from=array(), bool $gx2cmsTmpl=true)
+    public function __construct(bool $isHTML, array $from, array $to, string $subject, string $message, array $data=array(), string $templatingEngine='')
     {
-        $this->gx2cmsTmpl = $gx2cmsTmpl;
-        $this->prepareEnvelop($to, $tpl, $data, $subject, $from);
+        $this->templatingEngine = $templatingEngine;
+        $this->data = $data;
+        $this->envelop = new Envelop(null, $isHTML);
+        $this->prepareEnvelop($from, $to, $subject, $message);
     }
 
-    private function prepareEnvelop(array $from, array $to, string $subject, string $tpl, array $data=array())
+    public function setTemplatingEngine(string $s) {$this->templatingEngine = $s;}
+    public function setData(array $data) {$this->data = $data;}
+
+    public function setOpenSSLPkcs7Encrypt(bool $b) {$this->opensslPkcs7Encrypt = $b;}
+    public function setSSLCertificate(string $s) {$this->sslCertificate = $s;}
+    public function setSSLKey(string $s) {$this->sslKey = $s;}
+    public function setPrivateKeyPassword(string $s) {$this->privateKeyPassword = $s;}
+    public function setSSLCertChain(string $s) {$this->sslCertChain = $s;}
+
+    public function setReplyTo(string $email, string $name='') {$this->envelop->replyTo($email, $name);}
+    public function addCC(string $addr, string $name) {$this->envelop->addCC($addr, $name);}
+    public function addBCC(string $addr, string $name) {$this->envelop->addBCC($addr, $name);}
+    public function addAttachment(string $addr, string $name){$this->envelop->addAttachment($addr, $name);}
+
+
+    private function prepareEnvelop(array $from, array $to, string $subject, string $emailMessage)
     {
-        if (((isset($to[0]) && isset($to[0][self::KEY_TO_EMAIL])) || isset($to[self::KEY_TO_EMAIL])) && isset($from[self::KEY_TO_EMAIL]) && $subject && $tpl)
+        if (((isset($to[0]) && isset($to[0][self::KEY_TO_EMAIL])) || isset($to[self::KEY_TO_EMAIL])) && isset($from[self::KEY_TO_EMAIL]) && $subject && $emailMessage)
         {
-            if ($this->gx2cmsTmpl) {
+            if ($this->templatingEngine === 'gx2cms') {
                 $ezpzTmpl = new GX2CMS\TemplateEngine\GX2CMS();
-                $emailMessage = $ezpzTmpl->compile(new GX2CMS\TemplateEngine\Model\Context($data), new GX2CMS\TemplateEngine\Model\Tmpl($tpl));
+                $emailMessage = $ezpzTmpl->compile(new GX2CMS\TemplateEngine\Model\Context($this->data), new GX2CMS\TemplateEngine\Model\Tmpl($message));
             }
-            else {
-                $emailMessage = $tpl;
+            else if ($this->templatingEngine === 'handlebars') {
+                $emailMessage = Hbs::render($message, $this->data);
             }
 
-            $this->envelop = new Envelop(null);
-            $this->envelop->from($from[self::KEY_FROM_ADDRESS], isset($from[self::KEY_FROM_NAME])?$from[self::KEY_FROM_NAME]:'');
+            $this->envelop->from($from[self::KEY_FROM_ADDRESS], isset($from[self::KEY_FROM_NAME])?$from[self::KEY_FROM_NAME]:'', true);
             if (isset($to[0]) && isset($to[0][self::KEY_TO_EMAIL])) {
                 foreach ($to as $toEmail) {
                     $this->envelop->to($toEmail[self::KEY_TO_EMAIL], $toEmail[self::KEY_TO_NAME]);
@@ -45,6 +68,11 @@ final class Mail
             }
             $this->envelop->subject($subject);
             $this->envelop->message($emailMessage);
+
+            if ($this->opensslPkcs7Encrypt && $this->sslKey && $this->sslCertificate && $this->sslCertChain)
+            {
+                $this->envelop->sign($this->sslCertificate, $this->sslKey, $this->privateKeyPassword, $this->sslCertChain);
+            }
         }
     }
 
