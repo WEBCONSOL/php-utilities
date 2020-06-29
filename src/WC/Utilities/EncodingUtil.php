b7d9221826aae1360248832ba0f35af1dbafe618
Sothea Nim <sothea.nim@ezpizee.com>
2020-06-28 23:52:00 -0400

    improved json validation

--- src/WC/Utilities/EncodingUtil.php
+++ src/WC/Utilities/EncodingUtil.php
@@ -7,17 +7,7 @@ class EncodingUtil
     public static function isBase64Encoded($val): bool {return is_string($val) && base64_encode(base64_decode($val, true)) === $val;}
 
     public static function isValidJSON($str): bool {
-        if (is_string($str) && $str) {
-            $str = trim($str);
-            $first = $str ? $str[0] : '';
-            $last = $str ? $str[strlen($str) - 1] : '';
-            if (($first === "{" && $last === "}") || ($first === "[" && $last === "]")) {
-                if (is_string($str) && is_array(json_decode($str, true)) && (json_last_error() == JSON_ERROR_NONE)) {
-                    return true;
-                }
-            }
-        }
-        return false;
+        return $str && is_string($str) && is_array(json_decode($str, true)) && (json_last_error() == JSON_ERROR_NONE);
     }
 
     public static function isValidMd5($md5 =''): bool{return preg_match('/^[a-f0-9]{32}$/', $md5) ? true : false;}
