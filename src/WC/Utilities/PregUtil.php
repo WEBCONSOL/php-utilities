74f59d6d592c62a81a9dc77da2758809df731faa
Sothea Nim <sothea.nim@ezpizee.com>
2020-07-11 15:27:13 -0400

    added get images

--- src/WC/Utilities/PregUtil.php
+++ src/WC/Utilities/PregUtil.php
@@ -9,7 +9,7 @@ final class PregUtil
     public static function getMatches(string $pattern, string $subject): array {
         try {
             $matches = array();
-            preg_match_all($pattern, $subject, $matches);
+            preg_match_all($pattern, $subject, $matches, PREG_SET_ORDER);
             if (self::matchesFound($matches)) {
                 return $matches;
             }
@@ -23,5 +23,34 @@ final class PregUtil
             return true;
         }
         return false;
+    }
+
+    public static function getImages(string $htmlString, bool $getAttrs = FALSE)
+    {
+        $postImages = array();
+
+        // Get all images
+        preg_match_all("<img (.+)>", $htmlString, $image_matches, PREG_SET_ORDER);
+
+        // Loop the images and add the raw img html tag to $postImages
+        foreach ($image_matches as $image_match)
+        {
+            $image = ['html'=>$image_match[0]];
+
+            // If attributes have been requested get them and add them to the $image
+            if ($getAttrs)
+            {
+                preg_match_all('/\s+?(.+)="([^"]*)"/U', $image_match[0], $image_attr_matches, PREG_SET_ORDER);
+
+                foreach ($image_attr_matches as $image_attr)
+                {
+                    $image['attr'][$image_attr[1]] = $image_attr[2];
+                }
+            }
+
+            $postImages[] = $image;
+        }
+
+        return $postImages;
     }
 }
